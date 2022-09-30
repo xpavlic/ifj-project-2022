20acdefe56b2bbe1ac03391d45bd4d3eb915e16a
xnekul04 <94606205+Unanddar@users.noreply.github.com>
2022-09-30 22:33:48 +0900

    init

---
+++ allinone.c
@@ -0,0 +1,246 @@
+
+#include "htab.h"
+#include "structs.c"
+
+
+
+htab_pair_t * htab_lookup_add(htab_t * t, htab_key_t key)
+{
+    if(t==NULL || key == NULL)
+        return NULL;
+    size_t position = (htab_hash_function(key)% t->arr_size);
+    htab_item_t * last = NULL;
+    //check if already existts
+    for(htab_item_t * i = t->arr_ptr[position];i!=NULL;i=i->next)
+    {
+        if(!strcmp(key,i->data.key))
+        {
+            i->data.value++;
+            return &(i->data);
+        }
+        last = i;
+    }
+    //if doesnt exists
+    //create new item
+    htab_item_t * newitem = malloc(sizeof(htab_item_t));
+    if(newitem == NULL)
+        return NULL;
+    newitem->data.key = malloc(strlen(key)+1);
+    if(newitem->data.key == NULL)
+        return NULL;
+    //setup new item
+    strcpy((char*)newitem->data.key,(char*)key);
+    newitem->next=NULL;
+    newitem->data.value=1;
+
+    //place new item do seznamu
+    if(t->arr_ptr[position] == NULL)
+    {
+        t->arr_ptr[position] = newitem;
+    }
+    else
+    {
+        last->next = newitem;
+    }
+    t->size++;
+    //resize
+    if(t->size/t->arr_size > AVG_LEN_MAX)
+    {
+        htab_resize(t,2*t->arr_size);
+        return htab_find(t,key);
+    }
+    
+    return &(newitem->data);
+
+}
+
+
+//File::Bucket
+size_t htab_bucket_count(const htab_t * t)     // velikost pole
+{
+    if(t==NULL)
+        return 0;
+    return t->arr_size;
+}
+
+//File::Clear
+
+void htab_clear(htab_t * t)    // ruší všechny záznamy
+{
+    htab_item_t * start = NULL;
+    htab_item_t * tmpnext = NULL;
+    (void)tmpnext;
+    for(size_t i = 0;i<t->arr_size;i++)
+    {
+        start = t->arr_ptr[i];
+        if(start == NULL)
+            continue;
+        for(htab_item_t * j = start;j!=NULL;)
+        {
+            htab_item_t * tmpnext = j->next;
+            free((char*)j->data.key);
+            free(j);
+            j = tmpnext;
+        }
+        t->arr_ptr[i] = NULL;
+
+    }
+}
+
+//File::erase
+
+bool htab_erase(htab_t * t, htab_key_t key)    // ruší zadaný záznam
+{
+    if(t==NULL|| key == NULL)
+        return 0;
+
+
+    
+    htab_item_t * previous = NULL;
+    size_t position = (htab_hash_function(key)% t->arr_size);
+    for(htab_item_t * i = t->arr_ptr[position];i!=NULL;i=i->next)
+    {
+        
+        if(!strcmp(key,i->data.key))
+            {
+            free((char*)i->data.key);
+            if(previous==NULL)
+                {t->arr_ptr[position]=i->next;}
+            else
+                {previous->next = i->next;}
+            free(i);
+
+            t->size--;
+
+            //resize
+            if(t->size/t->arr_size < AVG_LEN_MIN && t->size !=0 && t->size !=1)
+            {
+                htab_resize(t,t->arr_size/2);
+            }
+
+
+            return 1;
+            }
+        previous = i;
+    }
+    return 0;
+}
+
+//File::find
+htab_pair_t * htab_find(htab_t * t, htab_key_t key)  // hledání
+{
+    if(t==NULL || key == NULL)
+        return NULL;
+    size_t position = (htab_hash_function(key)% t->arr_size);
+    for(htab_item_t * i = t->arr_ptr[position];i!=NULL;i=i->next)
+    {
+        if(!strcmp(key,i->data.key))
+            return &(i->data);
+    }
+    return NULL;
+}
+
+//File::foreach
+void htab_for_each(const htab_t * t, void (*f)(htab_pair_t *data))
+{
+    htab_item_t * start;
+    for(size_t i = 0;i<t->arr_size;i++)
+    {
+        start = t->arr_ptr[i];
+        for(htab_item_t * j = start;j!=NULL;j=j->next)
+        {
+            f(&(j->data));
+        }
+    }
+}
+
+
+void htab_free(htab_t * t)     // destruktor tabulky
+{
+    htab_clear(t);
+    free(t->arr_ptr);
+    free(t);
+}
+
+
+size_t htab_hash_function(const char *str) {
+    if(str==NULL)
+        return 0;
+
+  uint32_t h=0;     // musí mít 32 bitů
+  const unsigned char *p;
+  for(p=(const unsigned char*)str; *p!='\0'; p++)
+      h = 65599*h + *p;
+  return h;
+}
+
+
+htab_t *htab_init(size_t n)               
+{
+    if(n<MIN_TABLE_SIZE)
+        n = MIN_TABLE_SIZE;
+
+    htab_t * mytab = malloc(sizeof(htab_t));
+    if(mytab==NULL)
+        return NULL;
+    mytab->size = 0;
+    mytab->arr_size=n;
+    mytab->arr_ptr=NULL;
+    
+    mytab->arr_ptr = calloc(n,sizeof( htab_item_t *));
+    if(mytab->arr_ptr == NULL)
+    {
+        free(mytab);
+        return NULL;
+    }
+    return mytab;
+}
+
+
+void htab_resize(htab_t *t, size_t newn)       // změna velikosti pole
+{         
+    //arg check                                     // (umožňuje rezervaci místa)
+    if (t == NULL)
+        return;
+    if(newn<MIN_TABLE_SIZE)
+        newn = MIN_TABLE_SIZE;
+    
+    //mallock
+    htab_t * newt = htab_init(newn);
+    if(newt == NULL)
+        return;
+
+    htab_item_t * start = NULL;
+    size_t position = 0;
+    (void)position;
+    htab_pair_t * change = NULL;
+    
+    //through ptr array
+    for(size_t i = 0;i<t->arr_size;i++)
+    {
+        start = t->arr_ptr[i];
+        //through seznam
+        for(htab_item_t * j = start;j!=NULL;j=j->next)
+        {
+            
+            change = htab_lookup_add(newt,j->data.key);
+            change->value=j->data.value;
+        }
+    }
+    //remove old tab
+    htab_clear(t);
+    free(t->arr_ptr);
+    t->arr_ptr=newt->arr_ptr;
+    free(newt);
+    t->arr_size=newn;
+    return;
+}
+
+
+size_t htab_size(const htab_t * t)             // počet záznamů v tabulce
+{
+    if(t==NULL)
+        return 0;
+    return t->size;
+}
+
---
+++ structs.c
@@ -0,0 +1,27 @@
+
+#ifndef __HTAB_STRUCTS__
+#define __HTAB_STRUCTS__
+
+#include "htab.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+#define MIN_TABLE_SIZE 10
+#define AVG_LEN_MAX 10
+#define AVG_LEN_MIN 5
+
+typedef struct htab_item{
+    htab_pair_t data;
+    struct htab_item * next;
+} htab_item_t;
+
+
+struct htab{
+    size_t size;
+    size_t arr_size;
+    htab_item_t ** arr_ptr;
+};
+
+
+
+#endif
