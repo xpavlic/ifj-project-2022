84feb121067432a80369c9aeb45682d4c0f58234
xnekul04 <94606205+Unanddar@users.noreply.github.com>
2022-10-03 21:06:43 +0900

    comments fix

--- htab.h
+++ htab.h
@@ -1,52 +1,105 @@
 
 // htab.h -- rozhraní knihovny htab (řešení IJC-DU2)
 // Licence: žádná (Public domain)
+// Autor: Štěpán Nekula
 
-// následující řádky zabrání násobnému vložení:
+
 #ifndef __HTAB_H__
 #define __HTAB_H__
 
 #include <string.h>     // size_t
 #include <stdbool.h>    // bool
 
-// Tabulka:
-struct htab;    // neúplná deklarace struktury - uživatel nevidí obsah
-typedef struct htab htab_t;     // typedef podle zadání
+// Table:
+struct htab;    
+typedef struct htab htab_t; 
 
 // Typy:
-typedef const char * htab_key_t;        // typ klíče
-typedef int htab_value_t;               // typ hodnoty
+typedef const char * htab_key_t;        // type of key
+typedef int htab_value_t;               // type of value
 
-// Dvojice dat v tabulce:
+/**
+ * @brief pair of key and value
+*/
 typedef struct htab_pair {
-    htab_key_t    key;          // klíč
-    void * value;               // asociovaná hodnota
-} htab_pair_t;                  // typedef podle zadání
+    htab_key_t    key;          // key
+    void * value;               // associated value
+} htab_pair_t;                  // typedef
 
-// Rozptylovací (hash) funkce (stejná pro všechny tabulky v programu)
-// Pokud si v programu definujete stejnou funkci, použije se ta vaše.
+/**
+ * @brief hash function for htab
+ * @param htab_key_t str
+ * @return size_t
+*/
 size_t htab_hash_function(htab_key_t str);
 
-// Funkce pro práci s tabulkou:
+// Functions for manimulating table:
+
+/**
+ * @brief htable constructor
+ * @param size_t n
+ * @return htab_t*
+*/
 htab_t *htab_init(size_t n);                    // konstruktor tabulky
+
+/**
+ * @brief returns number of records in table
+ * @param const htab_t * t
+ * @return size_t
+*/
 size_t htab_size(const htab_t * t);             // počet záznamů v tabulce
+
+/**
+ * @brief returns size of array
+ * @param const htab_t * t
+ * @return size_t
+*/
 size_t htab_bucket_count(const htab_t * t);     // velikost pole
+
+/**
+ * @brief changes size of table
+ * @param htab_t *t
+ * @param size_t newn
+*/
 void htab_resize(htab_t *t, size_t newn);       // změna velikosti pole
                                                 // (umožňuje rezervaci místa)
-
+/**
+ * @brief finds pair in table
+ * @param const htab_t * t
+ * @param htab_key_t key
+ * @return htab_pair_t *
+*/
 htab_pair_t * htab_find(htab_t * t, htab_key_t key);  // hledání
-htab_pair_t * htab_lookup_add(htab_t * t, htab_key_t key);
 
-bool htab_erase(htab_t * t, htab_key_t key);    // ruší zadaný záznam
+/**
+ * @brief adds pair into table
+ * @param const htab_t * t
+ * @param htab_key_t key
+ * @return htab_pair_t *
+*/
+htab_pair_t * htab_lookup_add(htab_t * t, htab_key_t key);
 
 // for_each: projde všechny záznamy a zavolá na ně funkci f
 // Pozor: f nesmí měnit klíč .key ani přidávat/rušit položky
 void htab_for_each(const htab_t * t, void (*f)(htab_pair_t *data));
 
+/**
+ * @brief removes all records in table
+ * @param htab_t * t
+ * @param void (*f)(void*)
+*/
 void htab_clear(htab_t * t,void (*f)(void*));    // ruší všechny záznamy
+
+/**
+ * @brief table destructor
+ * @param htab_t * t
+ * @param void (*f)(void*)
+*/
 void htab_free(htab_t * t,void (*f)(void*));     // destruktor tabulky
 
-
+/**
+ * @brief does nothing
+*/
 void nope(){} //does nothing
 
 
--- symtable.h
+++ symtable.h
@@ -1,31 +1,77 @@
-#ifndef __SYMTABLE_H__
-#define __SYMTABLE_H__
-
 /*
 
+Table of Symbols
+
 HOW TO USE
-- give free function to symtable_init how do you want your structure freed
+- give destructor of used structure as value to symtable_init 
 - returned pair from symtable_add/symtable_find has 2 attributes "key" and "value"
-        - value is void* pointer where you can save/put your created structure
+        - value is void* pointer where you can save/put adress of your created structure
 
 */
+
+#ifndef __SYMTABLE_H__
+#define __SYMTABLE_H__
 
 #include "htab.c"
 #include "structs.c"
 
-// SYMTABLE
+/**
+ * @brief returned structure from symtable
+*/ 
+typedef struct htab_pair {
+    htab_key_t    key;          // key
+    void * value;               // associated value
+} htab_pair_t;                  // typedef
+
+/**
+ * @brief symtable constructor, sets function for later data deallocation
+ * @param void (*func)(void*)
+ * @return struct symtable *
+*/
 struct symtable * symtable_init(void (*func)(void*));
 
-void symtable_add_frame(struct symtable * st);
+/**
+ * @brief Adds new frame to the top of symtable
+ * @param struct symtable * t
+ * @return void
+*/
+void symtable_add_frame(struct symtable * t);
 
+/**
+ * @brief Removes top frame from symtable
+ * @param struct symtable * t
+ * @return void
+*/
 void symtable_remove_frame(struct symtable * t);
 
+/**
+ * @brief Removes all frames from symtable
+ * @param struct symtable * t
+ * @return void
+*/
 void symtable_clear(struct symtable * t);
 
+/**
+ * @brief table destructor
+ * @param struct symtable * t
+ * @return void
+*/
 void symtable_free(struct symtable * t);
 
+/**
+ * @brief Finds htab_pair_t by string key
+ * @param struct symtable * t
+ * @param htab_key_t key
+ * @return htab_pair_t *
+*/
 htab_pair_t *symtable_find(struct symtable *t, htab_key_t key);
 
+/**
+ * @brief Adds new htab_pair_t with given key
+ * @param struct symtable * t
+ * @param htab_key_t key
+ * @return htab_pair_t *
+*/
 htab_pair_t * symtable_add(struct symtable *t, htab_key_t key);
 
 #endif // __SYMTABLE_H__
